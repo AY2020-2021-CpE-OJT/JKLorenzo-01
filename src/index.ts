62315f69064f4514aa59244d40d0739d00f83625
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-17 14:26:16 +0800

    update output to autoformat numbers to add

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { add } from "./modules/math.js";
 
 function main() {
-  const num1 = 1;
-  const num2 = 7;
-  const num3 = 15;
-  const sum = add([num1, num2, num3]);
-  console.log(`The sum of ${num1}, ${num2}, and ${num3} is ${sum}.`);
+  const numbers_to_add = [1, 7, 15];
+
+  const sum = add(numbers_to_add);
+
+  const formatted_nums_to_add = numbers_to_add
+    .map((num, index) => {
+      if (numbers_to_add.length === index + 1) {
+        return `and ${num}`;
+      }
+      return `${num},`;
+    })
+    .join(" ");
+
+  console.log(`The sum of ${formatted_nums_to_add} is ${sum}.`);
 }
 
 main();
